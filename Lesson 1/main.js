fc7d00916d1c0b817acd244d1198229b84a48b70
makima-god <kirillw1lly@gmail.com>
2021-07-19 20:01:12 +0300

    add lesson 1

---
+++ Lesson 1/main.js
@@ -0,0 +1,47 @@
+/* var msg = 'It is good';
+var num = 15;
+
+console.log(typeof(msg));
+console.log(typeof(num)); */
+
+/* var a = 12;
+var b = 5;
+
+var c = (a + b);
+
+console.log(c);
+
+var remains = a % b;
+
+console.log(remains);
+
+var d = 2 ** 3;
+
+console.log(d);
+
+var a = 2;
+
+a += 1;
+a *= 3;
+a /= 2;
+
+console.log(a); */
+
+/* var msg = 'Привет, ';
+var name = 'Кирилл';
+var msg2 = ', как дела?';
+
+var result = msg + name + msg2;
+
+var specialString = 
+`
+    Lorem
+    Ipsum
+`;
+
+console.log(result);
+console.log(specialString); */
+
+console.log(50 > 30);
+console.log(50 < 30);
+console.log(50 <= 49);
