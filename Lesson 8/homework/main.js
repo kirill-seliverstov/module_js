fbba39c5d809de689d918e16cbca37d5e92c16f2
unknown <upmewakelvmpls@gmail.com>
2021-08-16 20:32:53 +0300

    some homework

---
+++ Lesson 8/homework/main.js
@@ -0,0 +1,33 @@
+// 
+// Задание №1
+// 
+
+function counterFactory(start, step) {
+    step = 0;
+
+    function tictoc() {
+        step++;
+        return start + step;
+    }
+
+    return tictoc;
+}
+
+// let counter = counterFactory(3);
+// console.log(counter());
+// console.log(counter());
+// console.log(counter());
+// console.log(counter());
+
+
+// 
+// Задание №2
+// 
+
+function take(tictoc, x) {
+    let arr = [];
+    do {
+        
+    } while(x);
+}
+
