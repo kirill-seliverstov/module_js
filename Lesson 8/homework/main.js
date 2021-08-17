c2510f30423ac33bb97fec965d76b49d6b78ef19
unknown <upmewakelvmpls@gmail.com>
2021-08-17 17:26:05 +0300

    lesson 8 task 3 done right

--- Lesson 8/homework/main.js
+++ Lesson 8/homework/main.js
@@ -49,7 +49,7 @@ let test = take(100, function(x) {
 let str = 'Разбейте текст этой задачи на отдельные слова, удаляя по пути точки и запятые, а полученные слова сложите в массив. Напишите функцию, которая возвращает массив из тех же слов, но развёрнутых задом наперёд, причём массив должен быть отсортирован по количеству букв в слове. Напишите другую функцию, которая считает общее количество букв «с» во всех элементах массива.'
 let newStr = str.replace(/[.,\s]/g, ' ').split('').reverse().join('');
 
-console.log(sortDependingOnLetters(newStr));
+letterC();
 
 function sortDependingOnLetters(str) {
     return str.split(' ').sort(sorting);
@@ -61,10 +61,10 @@ function sorting(first, second) {
     else return 0;
 };
 
-function letterC(str) {
+function letterC() {
     let count = 0;
-    str.split('');
-    str.forEach(function(elem) {
+    let newStr2 = str.split('');
+    newStr2.forEach(function(elem) {
         if (elem == 'с' || elem == 'С') {
             count++;
         } 
--- Lesson 8/homework/task-3-tests.js
+++ Lesson 8/homework/task-3-tests.js
@@ -24,4 +24,40 @@ function letterC(str) {
     console.log(count);
 };
 
-letterC(newStr);
+let str = 'Разбейте текст этой задачи на отдельные слова, удаляя по пути точки и запятые, а полученные слова сложите в массив. Напишите функцию, которая возвращает массив из тех же слов, но развёрнутых задом наперёд, причём массив должен быть отсортирован по количеству букв в слове. Напишите другую функцию, которая считает общее количество букв «с» во всех элементах массива.'
+
+// let regex = /[.,\s]/g;
+
+// let newStr = str.replace(regex, '');
+// console.log(newStr);
+
+// let newStr = str.split(' ');
+// console.log(newStr);
+
+
+// let newStr = str.replace(/\./g,'');
+// let newStr2 = newStr.replace(/\,/g, '');
+// console.log(newStr2.split(' '));
+// console.log(newStr2.split(' ').reverse().sort(sorting));
+
+// console.log(str.replace(/[.,\s]/g, ' ').split('').reverse().join(''));
+
+function sorting(first, second) {
+    if (first.length > second.length) return 1;
+    else if (first.length < second.length) return -1;
+    else return 0;
+};
+// console.log(newStr2.split('').reverse().join(''));
+
+function newStr(str) {
+    let arr = str.replace(/[.,\s]/g, ' ').split('').reverse().join('');
+    console.log(arr);
+}
+
+function sorta(str) {
+    return str.split(' ').sort(sorting);
+}
+
+newStr(str);
+console.log(sorta(str));
+
